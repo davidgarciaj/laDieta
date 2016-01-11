024e117a2332c80bce04aabf69fa24703b1c4f9e
DavidG <igdrasilenllamas@gmail.com>
2016-01-11 11:56:45 +0100

    Añadidos metodos getter y metodo que compara las calorias de dos usuarios

--- Usuario.java
+++ Usuario.java
@@ -9,17 +9,17 @@ public class Usuario
 {
     // Guarda nombre del usuario
     private String nombre;
-	//Guarda primer apellido
+    //Guarda primer apellido
     private String apellido1;
-	//Guarda segundo apellido
+    //Guarda segundo apellido
     private String apellido2;
-	//Guarda gramos de proteinas ingeridas
+    //Guarda gramos de proteinas ingeridas
     private float proteinas;
-	//Guarda gramos de grasas ingeridas
+    //Guarda gramos de grasas ingeridas
     private float grasas;
-	//Guarda gramos de carbohidratos ingeridas
+    //Guarda gramos de carbohidratos ingeridas
     private float carbohidratos;
-	//Guarda calorias ingeridas
+    //Guarda calorias ingeridas
     private float calorias;
 
     /**
@@ -42,7 +42,21 @@ public class Usuario
         //calorias digeridas por el usuario
         calorias = 0;
     }
-
+    
+    /**
+     * Devuelve el nombre del Usuario 
+     */
+    public String getNombreCompleto(){
+        return nombre + " " + apellido1 + " " + apellido2;
+    }
+    
+    /**
+     * Devuelve calorias consumidas por el Usuario
+     */
+    public float getCalorias(){
+        return calorias;
+    }
+    
     /**
      * Darle a comer una cantidad de un alimento
      */
@@ -58,27 +72,42 @@ public class Usuario
      * Mostrar datos del consumo del usuario
      */
     public void mostrarDatos(){
-		float totalMacronutrientes = grasas + proteinas + carbohidratos;
+        float totalMacronutrientes = grasas + proteinas + carbohidratos;
         System.out.println(nombre + " " + apellido1 + " " + apellido2);
         System.out.println();
-		if(proteinas != 0){
-			System.out.println("Gramos totales de proteinas ingeridos: " + proteinas + " (" + proteinas/totalMacronutrientes*100 + "%)");
-		}
+        if(proteinas != 0){
+            System.out.println("Gramos totales de proteinas ingeridos: " + proteinas + " (" + proteinas/totalMacronutrientes*100 + "%)");
+        }
         else{
-			System.out.println("Gramos totales de proteinas ingeridos: " + proteinas);
-		}
-		if(grasas != 0){
-			System.out.println("Gramos totales de proteinas ingeridos: " + grasas + " (" + grasas/totalMacronutrientes*100 + "%)");
-		}
+            System.out.println("Gramos totales de proteinas ingeridos: " + proteinas);
+        }
+        if(grasas != 0){
+            System.out.println("Gramos totales de proteinas ingeridos: " + grasas + " (" + grasas/totalMacronutrientes*100 + "%)");
+        }
         else{
-        	System.out.println("Gramos totales de grasas ingeridas: " + grasas);
-    	}	
-		if(carbohidratos != 0){
-			System.out.println("Gramos totales de proteinas ingeridos: " + carbohidratos + " (" + carbohidratos/totalMacronutrientes*100 + "%)");
-		}
+            System.out.println("Gramos totales de grasas ingeridas: " + grasas);
+        }   
+        if(carbohidratos != 0){
+            System.out.println("Gramos totales de proteinas ingeridos: " + carbohidratos + " (" + carbohidratos/totalMacronutrientes*100 + "%)");
+        }
         else{
-        	System.out.println("Gramos totales de carbohidratos ingeridos: " + carbohidratos);
-    	}
+            System.out.println("Gramos totales de carbohidratos ingeridos: " + carbohidratos);
+        }
         System.out.println("Calorias totales ingeridas: " + calorias);
+    }
+    
+    /**
+     * Compara la ingesta de calorias de este usuario con otro usuario dado
+     */
+    public void comparaCalorias(Usuario usuarioAComparar){
+        float caloriasSegundoUsuario = usuarioAComparar.getCalorias();
+        String masCalorias = usuarioAComparar.getNombreCompleto() + " a consumido mas calorias que "+ getNombreCompleto();
+        if(calorias > caloriasSegundoUsuario){
+            masCalorias = getNombreCompleto() + " a consumido mas calorias que "+ usuarioAComparar.getNombreCompleto();
+        }
+        else if(calorias == caloriasSegundoUsuario){
+            masCalorias = "Tanto "+ getNombreCompleto() + " como "+ usuarioAComparar.getNombreCompleto() + " han consumido las mismas calorias.";
+        }
+        System.out.println(masCalorias);
     }
 }
